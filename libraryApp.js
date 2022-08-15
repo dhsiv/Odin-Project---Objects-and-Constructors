8a6621de183a6b4d9e00fd2b11389a3068c303e0
David Sadler IV <dhsiv82@gmail.com>
2022-08-14 23:13:09 -0400

    - added for loop - moving on to build and manipulate DOM

--- libraryApp.js
+++ libraryApp.js
@@ -9,3 +9,11 @@ const books = {
     return "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet";
   },
 };
+
+let myLibrary = ["The Hobbit", "The Four Agreements"];
+
+function Book() {}
+
+function addBookToLibrary() {
+  for (i = 0; myLibrary.length < 2; i++) {}
+}
